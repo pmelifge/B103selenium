e60a842f1d1884388e5032b959935ab800b4794b
pmelifge <109210047+pmelifge@users.noreply.github.com>
2022-12-20 16:09:02 +0200

    10-12

--- src/test/java/day02_DriverMethods/C04_ClassWork.java
+++ src/test/java/day02_DriverMethods/C04_ClassWork.java
@@ -1,9 +1,14 @@
 package day02_DriverMethods;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 
 public class C04_ClassWork {
     public static void main(String[] args) {
@@ -14,9 +19,19 @@ public class C04_ClassWork {
         //a.web sayfasına gidin. https://www.amazon.com/
         driver.get("https://www.amazon.com/");
 
-//Search(ara) "city bike"
-
-//Amazon'da görüntülenen ilgili sonuçların sayısını yazdırın
-//Sonra karşınıza çıkan ilk sonucun resmine tıklayın.
+//Search(ara) “city bike”
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("city bike", Keys.ENTER);
+        //Amazon'da görüntülenen ilgili sonuçların sayısını yazdırın
+        List<WebElement> aramaSonucYazisi = driver.findElements(By.className("sg-col-inner"));
+        System.out.println("Arama Sonuc Yazisi = " + aramaSonucYazisi.get(0).getText());
+        // Arama sonuc sayisini konsola yazdiriniz
+        String[] sonucSayisi = aramaSonucYazisi.get(0).getText().split(" ");
+        System.out.println("Sonuc Sayisi = " + sonucSayisi[2]);
+        // Sonuc sayisini Lambda ile yazdiriniz
+        Arrays.stream(aramaSonucYazisi.get(0).getText().split(" ")).limit(3).skip(2).forEach(System.out::println);
+        //Sonra karşınıza çıkan ilk sonucun resmine tıklayın.
+        List<WebElement> ilkWE = driver.findElements(By.className("s-image"));
+        ilkWE.get(0).click();
     }
 }
+
